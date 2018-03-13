a54a25e0a4658dfe0d5dd7055fb398a196422e18
sunhongjian <sunhj@huntor.cn>
2018-03-13 09:42:22 +0800

    增加模型验证

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
 # . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: '哈利·波特与密室',
                description:
                    %{<p>
-<em>Native Apps, Multiple Platforms</em>
-Answer the question “Can we build this for ALL the devices?” with a
-resounding YES. This book will help you get there with a real-world
-introduction to seven platforms, whether you’re new to mobile or an
-experienced developer needing to expand your options. Plus, you’ll find
-out which cross-platform solution makes the most sense for your needs.
+<em>《哈利·波特与密室》是“哈利·波特”系列的第二部。</em>
+
+哈利·波特在霍格沃茨魔法学校学习一年之后，暑假开始了。他在姨父姨妈家熬过痛苦的假期。正当他准备打点行装去学校时，小精灵多比前来发出警告：如果哈利返回霍格沃茨，灾难将会临头。
+
+但哈利义无返顾地回到了霍格沃茨，新来的吉罗德·洛哈特教授装腔作势，让他作呕；游荡在女生舆洗室里的幽灵“哭泣的桃金娘”搅得他不得安宁；低年级的小女生金妮对他投来关切目光常令他尴尬不已；小男生科林·克里维“追星”式的跟踪又经常使他落荒而逃。
 </p>},
                image_url: '7apps.jpg',
                price: 26.00)
